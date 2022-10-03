db85090b5bacd66d9ee6df96bd9c98306fb47da8
LinTaklin <lintanaka@gmail.com>
2022-10-03 15:18:20 +0300

    added Canny ED, screen capture, auto ui generator

--- src/core/autogen_ui_opencv_params.py
+++ src/core/autogen_ui_opencv_params.py
@@ -2,6 +2,8 @@ import os
 from tkinter import *
 from tkinter import ttk
 from tkinter.ttk import Frame
+
+import mss
 from PIL import Image, ImageTk
 import cv2
 import numpy as np
@@ -18,11 +20,12 @@ def dir_path(string):
 
 
 def source_change(string):
-    if string == "webcam" or string==0:
+    if string == "webcam" or string == 0:
         return 0
+    elif string == "screen" or string == "monitor":
+        return 1
     else:
         return dir_path(string)
-
 
 parser = argparse.ArgumentParser(description='Auto generates UI from config file.')
 parser.add_argument('--param_config_path', type=dir_path, help='Path to config params.', default='ui.yaml')
@@ -32,8 +35,8 @@ args = parser.parse_args()
 params = yaml_reader(args.param_config_path)
 
 root = Tk()
-root.geometry("740x680")
-root.title(os.path.split(args.param_config_path)[-1].split(".")[0].replace("_"," "))
+root.geometry("980x680")
+root.title(os.path.split(args.param_config_path)[-1].split(".")[0].replace("_", " "))
 
 for i, (k, v) in enumerate(params.items()):
     row = i + 2
@@ -46,20 +49,37 @@ for i, (k, v) in enumerate(params.items()):
     v["scale"].grid(row=row, column=1, pady=0, padx=0)
 
 lmain = Label(root)
-lmain.grid(row=i+3, column=1, pady=0, padx=0)
+lmain.grid(row=i + 3, column=1, pady=0, padx=0)
 
-cap = cv2.VideoCapture(args.source)
+if args.source != 1:  # not monitor
+    cap = cv2.VideoCapture(args.source)
+else:
+    monitor = {
+        "top": 200,  # 100px from the top
+        "left": 991,  # 100px from the left
+        "width": 1868 - 991,
+        "height": 693 - 200,
+        "mon": 0,
+    }
 
 
 def show_frame():
-    ret, frame = cap.read()
-
-    img = Image.fromarray(frame[:, :, ::-1]).resize((640, 480))
+    if args.source != 1:
+        ret, frame = cap.read()
+    else:
+        img_byte = sct.grab(monitor)
+        frame = np.frombuffer(img_byte.rgb, np.uint8).reshape(monitor["height"], monitor["width"], 3)[:, :, ::-1]
+    img = Image.fromarray(frame[:, :, ::-1]).resize(frame.shape[:-1][::-1])
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
     lmain.configure(image=imgtk)
     lmain.after(10, show_frame)
 
 
-show_frame()  #Display
-root.mainloop()
+if args.source != 1:
+    show_frame()  # Display
+    root.mainloop()
+else:
+    with mss.mss() as sct:
+        show_frame()  # Display
+        root.mainloop()
