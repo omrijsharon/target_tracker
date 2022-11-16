87a56dcdec783d636cfbfd96bf7f4732c5f9d7cb
omrijsharon <omri_s@yahoo.com>
2022-11-16 12:18:28 +0200

    fuse sensors test

--- tests/fractal_filter.py
+++ tests/fractal_filter.py
@@ -8,14 +8,17 @@ def fractalize(img: np.ndarray, scaling_factors):
         return img
     img = img / 255
     # assert scaling_factors[0] != scaling_factors[1], "scaling factors must be different."
-    eps = np.finfo(np.float64).eps
-    scaling_factors.sort(reverse=True)
-    scaled_imgs = ([cv2.filter2D(img.copy(), -1, np.ones(shape=(s, s))) for s in scaling_factors])
+    # eps = np.finfo(np.float64).eps
+    # scaling_factors.sort()
+    scaled_imgs = ([np.clip(cv2.filter2D(img.copy(), -1, np.ones(shape=(s, s))), a_min=1, a_max=np.inf) for s in sorted(scaling_factors)])
     # log(N_0 / N_1) = dim * log(S_0 / S_1)
     # return np.log((scaled_imgs[0] / (scaled_imgs[1] + eps)) + eps) / np.log(scaling_factors[0] / scaling_factors[1])
-    return np.log((scaled_imgs[1] / (scaled_imgs[0] + eps)) + eps) / np.log(scaling_factors[1] / scaling_factors[0])
+    scaling_factors_ratio = scaling_factors[0] / scaling_factors[1]
+    # scaled_imgs_ratio = np.clip(scaled_imgs[0] / scaled_imgs[1], a_min=scaling_factors_ratio, a_max=1.0)
+    scaled_imgs_ratio = scaled_imgs[0] / scaled_imgs[1]
+    return np.log(scaled_imgs_ratio) / np.log(scaling_factors_ratio)
 
 
 def image_fractal(fractaled_img):
-    return (255 * fractaled_img / 2).astype(np.uint8)
+    return (255 * fractaled_img / fractaled_img.max()).astype(np.uint8)
 
